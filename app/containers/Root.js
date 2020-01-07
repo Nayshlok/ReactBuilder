04189911079ecb54bd91667e4ba00098d04eaaba
Nayshlok <borlandavid@gmail.com>
2020-01-07 07:11:08 -0700

    Remove Redux
    Not a big fan of redux especially with the React hooks, I find it less
    necessary

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,22 +1,13 @@
 // @flow
 import React from 'react';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
-import type { Store } from '../reducers/types';
 import Routes from '../Routes';
 
-type Props = {
-  store: Store,
-  history: {}
-};
-
-const Root = ({ store, history }: Props) => (
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Routes />
-    </ConnectedRouter>
-  </Provider>
+const Root = () => (
+  <Router>
+    <Routes />
+  </Router>
 );
 
 export default hot(Root);
